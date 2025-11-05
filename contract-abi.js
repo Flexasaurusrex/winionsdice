a2569b7a3c9659f08ae70a8695446daba864de90
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-11-05 09:44:13 -0800

    Create contract-abi.js

---
+++ contract-abi.js
@@ -0,0 +1,336 @@
+// Contract ABI for WinionsDistribution
+// Contract Address: 0xb4795Da90B116Ef1BD43217D3EAdD7Ab9A9f7Ba7
+
+const DISTRIBUTION_CONTRACT_ABI = [
+    {
+        "inputs": [{"internalType": "address","name": "_winionsContract","type": "address"}],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [{"internalType": "address","name": "owner","type": "address"}],
+        "name": "OwnableInvalidOwner",
+        "type": "error"
+    },
+    {
+        "inputs": [{"internalType": "address","name": "account","type": "address"}],
+        "name": "OwnableUnauthorizedAccount",
+        "type": "error"
+    },
+    {
+        "inputs": [],
+        "name": "ReentrancyGuardReentrantCall",
+        "type": "error"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {"indexed": true,"internalType": "address","name": "admin","type": "address"},
+            {"indexed": false,"internalType": "uint256","name": "tokenId","type": "uint256"}
+        ],
+        "name": "EmergencyWithdrawal",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {"indexed": false,"internalType": "string","name": "houseName","type": "string"},
+            {"indexed": false,"internalType": "uint256","name": "tokenId","type": "uint256"}
+        ],
+        "name": "HouseInventoryAdded",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {"indexed": true,"internalType": "address","name": "recipient","type": "address"},
+            {"indexed": true,"internalType": "uint256","name": "tokenId","type": "uint256"},
+            {"indexed": false,"internalType": "string","name": "houseName","type": "string"},
+            {"indexed": false,"internalType": "uint256","name": "rollTotal","type": "uint256"},
+            {"indexed": false,"internalType": "bool","name": "wasWhitelisted","type": "bool"}
+        ],
+        "name": "NFTDistributed",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {"indexed": true,"internalType": "address","name": "previousOwner","type": "address"},
+            {"indexed": true,"internalType": "address","name": "newOwner","type": "address"}
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {"indexed": false,"internalType": "string","name": "priceType","type": "string"},
+            {"indexed": false,"internalType": "uint256","name": "newPrice","type": "uint256"}
+        ],
+        "name": "PriceUpdated",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {"indexed": true,"internalType": "address","name": "buyer","type": "address"},
+            {"indexed": false,"internalType": "uint256","name": "numberOfRolls","type": "uint256"},
+            {"indexed": false,"internalType": "uint256","name": "amountPaid","type": "uint256"}
+        ],
+        "name": "RollsPurchased",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {"indexed": true,"internalType": "address","name": "user","type": "address"},
+            {"indexed": false,"internalType": "uint256","name": "freeRolls","type": "uint256"}
+        ],
+        "name": "WhitelistUpdated",
+        "type": "event"
+    },
+    {
+        "inputs": [
+            {"internalType": "string","name": "houseName","type": "string"},
+            {"internalType": "uint256[]","name": "tokenIds","type": "uint256[]"}
+        ],
+        "name": "addToHouseInventory",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {"internalType": "address[]","name": "addresses","type": "address[]"},
+            {"internalType": "uint256[]","name": "freeRolls","type": "uint256[]"}
+        ],
+        "name": "addToWhitelist",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {"internalType": "uint256","name": "rollTotal","type": "uint256"},
+            {"internalType": "string","name": "houseName","type": "string"}
+        ],
+        "name": "claimWinion",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "distributionActive",
+        "outputs": [{"internalType": "bool","name": "","type": "bool"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "string","name": "houseName","type": "string"}],
+        "name": "emergencyWithdrawHouse",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "uint256","name": "tokenId","type": "uint256"}],
+        "name": "emergencyWithdrawNFT",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "fiveRollPrice",
+        "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getAllHouses",
+        "outputs": [{"internalType": "string[]","name": "","type": "string[]"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getContractBalance",
+        "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "string","name": "houseName","type": "string"}],
+        "name": "getHouseInventory",
+        "outputs": [{"internalType": "uint256[]","name": "","type": "uint256[]"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "string","name": "houseName","type": "string"}],
+        "name": "getHouseInventoryCount",
+        "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getPrices",
+        "outputs": [
+            {"internalType": "uint256","name": "single","type": "uint256"},
+            {"internalType": "uint256","name": "three","type": "uint256"},
+            {"internalType": "uint256","name": "five","type": "uint256"}
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "address","name": "user","type": "address"}],
+        "name": "getUserRolls",
+        "outputs": [
+            {"internalType": "uint256","name": "freeRolls","type": "uint256"},
+            {"internalType": "uint256","name": "paidRolls","type": "uint256"}
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "string","name": "","type": "string"}],
+        "name": "houseExists",
+        "outputs": [{"internalType": "bool","name": "","type": "bool"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {"internalType": "string","name": "","type": "string"},
+            {"internalType": "uint256","name": "","type": "uint256"}
+        ],
+        "name": "houseInventory",
+        "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+        "name": "houseNames",
+        "outputs": [{"internalType": "string","name": "","type": "string"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [{"internalType": "address","name": "","type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "uint256","name": "numberOfRolls","type": "uint256"}],
+        "name": "purchaseRolls",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "address","name": "","type": "address"}],
+        "name": "purchasedRolls",
+        "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "renounceOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "uint256","name": "newPrice","type": "uint256"}],
+        "name": "setFiveRollPrice",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "uint256","name": "newPrice","type": "uint256"}],
+        "name": "setSingleRollPrice",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "uint256","name": "newPrice","type": "uint256"}],
+        "name": "setThreeRollPrice",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "singleRollPrice",
+        "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "threeRollPrice",
+        "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "toggleDistribution",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "address","name": "newOwner","type": "address"}],
+        "name": "transferOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {"internalType": "address","name": "user","type": "address"},
+            {"internalType": "uint256","name": "freeRolls","type": "uint256"}
+        ],
+        "name": "updateWhitelist",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "address","name": "","type": "address"}],
+        "name": "whitelist",
+        "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "winionsContract",
+        "outputs": [{"internalType": "contract IERC721","name": "","type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "withdrawETH",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+
+// Export for use in other files
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { DISTRIBUTION_CONTRACT_ABI };
+}
